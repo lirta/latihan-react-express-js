3cb5d99da7562c33bb8b33a261d4d352d0a6f45a
lirta <indalirta@gmail.com>
2020-08-29 03:20:29 +0700

    master

---
+++ server/controller/adminController.js
@@ -0,0 +1,72 @@
+const category = require("../models/Category");
+const Category = require("../models/Category");
+const { find } = require("../models/Category");
+
+module.exports = {
+  viewDashboar: (req, res) => {
+    res.render("admin/dashboard/view_dashboard");
+  },
+
+  viewCategory: async (req, res) => {
+    try {
+      const category = await Category.find();
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = { message: alertMessage, status: alertStatus };
+      res.render("admin/category/view_category", { category, alert });
+    } catch (error) {
+      res.redirect("/admin/category");
+    }
+  },
+  addCategory: async (req, res) => {
+    try {
+      const { name } = req.body;
+      // console.log(name);
+      await Category.create({ name });
+      req.flash("alertMessage", "Success Add Category");
+      req.flash("alertStatus", "success");
+      res.redirect("/admin/category");
+    } catch (error) {
+      req.flash("alertMessage", "$error.message");
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/category");
+    }
+  },
+  editCategory: async (req, res) => {
+    try {
+      const { id, name } = req.body;
+      const category = await Category.findOne({ _id: id });
+      category.name = name;
+      await category.save();
+      req.flash("alertMessage", "Success Update Category");
+      req.flash("alertStatus", "warning");
+      res.redirect("/admin/category");
+    } catch (error) {
+      req.flash("alertMessage", "$error.message");
+      req.flash("alertStatus", "danger");
+      res.redirect("/admin/category");
+    }
+  },
+  deleteCategory: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findOne({ _id: id });
+      await category.remove();
+      req.flash("alertMessage", "Success Delete Category");
+      req.flash("alertStatus", "success");
+      res.redirect("/admin/category");
+    } catch (error) {
+      res.redirect("/admin/category");
+    }
+  },
+
+  viewBank: (req, res) => {
+    res.render("admin/bank/view_bank");
+  },
+  viewItem: (req, res) => {
+    res.render("admin/item/view_item");
+  },
+  viewBooking: (req, res) => {
+    res.render("admin/booking/view_booking");
+  },
+};
